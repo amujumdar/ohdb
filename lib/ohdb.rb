185c42ba9b4c62c91545162f79e9a5d080eab4e0
Robin Luckey <robin@Tangier.local>
2009-02-24 16:35:02 -0800

    [FIX] Improvements to library loading

--- lib/ohdb.rb
+++ lib/ohdb.rb
@@ -2,16 +2,31 @@ module Ohdb
 end
 
 OHDB_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$: << OHDB_ROOT
 
 require 'rubygems'
-require 'ohcount'
-require 'scm'
+
+# The following Ohloh requires are a bit unusual to ease development.
+# First we attempt to load the library as a gem.
+# If that fails, we look for the lib in a directory parallel to the ohdb code tree.
+begin
+	require 'ohcount'
+rescue LoadError
+	require '../ohcount/lib/ohcount'
+end
+
+begin
+	require 'scm'
+rescue LoadError
+	require '../scm/lib/scm'
+end
+
 require 'sqlite3'
 require 'activerecord'
 
-require File.join(OHDB_ROOT, 'vendor/plugins/acts_as_list/init')
+require 'vendor/plugins/acts_as_list/init'
 
-require File.join(OHDB_ROOT, 'lib/ohdb/database')
-require File.join(OHDB_ROOT, 'lib/ohdb/migration')
-require File.join(OHDB_ROOT, 'lib/ohdb/commit')
-require File.join(OHDB_ROOT, 'lib/ohdb/import')
+require 'lib/ohdb/database'
+require 'lib/ohdb/migration'
+require 'lib/ohdb/commit'
+require 'lib/ohdb/import'
